874abab53a4125bd6ec08f4dde1b07acdcfed2a2
Kareem Khaleel <k.khaleel4@gmail.com>
2025-11-15 22:24:33 +0200

    Add Simple ASP.Net webAPI
    Signed-off-by: Kareem Khaleel <k.khaleel4@gmail.com>

---
+++ ASP-Net-Core/SimpleAPI/Program.cs
@@ -0,0 +1,58 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+// Add Root Endpoint That Returns a Welcome Message when Accessed at "/"
+app.MapGet("/", () => "Welcome to the Weather Forecast API!");
+
+app.MapPost("/doSomething", (string? input1, string? input2) =>
+{
+    if(string.IsNullOrEmpty(input1) || string.IsNullOrEmpty(input2))
+    {
+        return Results.BadRequest("Both input1 and input2 are required.");
+    }
+    var result = $"Processed inputs: {input1} and {input2}";
+    return Results.Ok(result);
+}).WithName("DoSomething")
+  .WithOpenApi();
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
